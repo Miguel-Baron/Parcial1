84b927dd75a09107825bcfd90bfa3f9d14d3bc51
Miguel-Baron <89749914+Miguel-Baron@users.noreply.github.com>
2023-03-12 23:40:33 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -16,7 +16,6 @@ def download_file():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                   '(KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
     }
-    
     response = requests.get(url, headers=headers)
 
     return response
